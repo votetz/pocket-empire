c13c93bdbc89d3c0408b64de11ce1d2c69358d28
Bohdan Poruchnik <bogomdannyibogdan@gmail.com>
2026-05-24 01:20:33 +0300

    feat: add colorization to ConsoleRender for better tile rendering

--- src/main/java/com/wrathborn/display/ConsoleRender.java
+++ src/main/java/com/wrathborn/display/ConsoleRender.java
@@ -1,6 +1,7 @@
 package com.wrathborn.display;
 
 import com.wrathborn.world.Map;
+import com.wrathborn.world.Tile;
 
 public class ConsoleRender {
     private final Map map;
@@ -12,9 +13,23 @@ public class ConsoleRender {
     public void render() {
         for (int y = 0; y < map.getHeight(); y++) {
             for (int x = 0; x < map.getWidth(); x++) {
-                System.out.print(map.getTile(x, y).getType().symbol);
+                Tile tile = map.getTile(x, y);
+                System.out.print(colorize(tile));
             }
             System.out.println();
         }
+    }
+
+    private String colorize(Tile tile) {
+        String color = switch (tile.getType()) {
+            case GRASS, PLAINS        -> AnsiColor.GREEN;
+            case FOREST, JUNGLE, TAIGA -> AnsiColor.DARK_GREEN;
+            case WATER, SWAMPS        -> AnsiColor.BLUE;
+            case OCEAN                -> AnsiColor.CYAN;
+            case DESERT, SAVANNA      -> AnsiColor.YELLOW;
+            case MOUNTAIN, CAVES      -> AnsiColor.GRAY;
+            case TUNDRA               -> AnsiColor.WHITE;
+        };
+        return color + tile.getType().symbol + AnsiColor.RESET;
     }
 }
