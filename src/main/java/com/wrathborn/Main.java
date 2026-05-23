b91722b335d9a9dc048a0e54216519cd15abf5e2
Bohdan Poruchnik <bogomdannyibogdan@gmail.com>
2026-05-24 00:11:34 +0300

    chore: adjust water tile probability and update enum values

--- src/main/java/com/wrathborn/Main.java
+++ src/main/java/com/wrathborn/Main.java
@@ -1,8 +1,30 @@
 package com.wrathborn;
 
-public class Main {
+import com.wrathborn.world.World;
+
+public class Main{
     public static void main(String[] args) {
-        System.out.println("Wrathborn Engine Initialized");
+        com.wrathborn.world.Map map = createSampleMap(30, 10);
+        World world = new World(map);
+
+        com.wrathborn.display.ConsoleRender renderer = new com.wrathborn.display.ConsoleRender(world.getMap());
+        com.wrathborn.display.StatsDisplay stats = new com.wrathborn.display.StatsDisplay();
+
+        renderer.render();
+        stats.displayStats();
+    }
+
+    private static com.wrathborn.world.Map createSampleMap(int width, int height) {
+        com.wrathborn.world.Tile[][] tiles = new com.wrathborn.world.Tile[width][height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                com.wrathborn.tiles.TileType type = (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                    ? com.wrathborn.tiles.TileType.WATER
+                    : com.wrathborn.tiles.TileType.GRASS;
+                tiles[x][y] = com.wrathborn.tiles.TileFactory.create(x, y, type);
+            }
+        }
+        return new com.wrathborn.world.Map(width, height, tiles);
     }
 
 }
