73ba15efac3b4c85b393a8fb9dbea49050ff18dd
Bohdan Poruchnik <bogomdannyibogdan@gmail.com>
2026-05-23 21:55:30 +0300

    chore: character set conversions for the console

--- src/main/java/com/wrathborn/tiles/TileType.java
+++ src/main/java/com/wrathborn/tiles/TileType.java
@@ -1,18 +1,26 @@
 package com.wrathborn.tiles;
 
 public enum TileType {
+    // terrestrial biomes
     GRASS   (1.0, false, '.'),
-    PLAINS  (1.2, false, '_'),
-    DESERT  (0.7, false, 'd'),
-    SAVANNA (0.8, false, 's'),
-    FOREST  (0.5, false, 'T'),
+    PLAINS  (1.2, false, '"'),
+    DESERT  (0.7, false, '~'),
+    SAVANNA (0.8, false, ','),
+
+    // types of forests
+    FOREST  (0.5, false, 'f'),
     JUNGLE  (0.3, false, 'J'),
-    TAIGA   (0.4, false, 't'),
-    TUNDRA  (0.6, false, 'u'),
+    TAIGA   (0.4, false, '▲'),
+    TUNDRA  (0.6, false, '*'),
+
+    // mountain
     MOUNTAIN(0.2, true,  '^'),
-    CAVES   (0.3, false, 'c'),
-    WATER   (0.0, true,  '~'),
-    OCEAN   (0.0, true,  'O');
+    CAVES   (0.3, false, '#'),
+
+    // water
+    WATER   (0.0, true,  '≈'),
+    OCEAN   (0.0, true,  '='),
+    SWAMPS  (0.2, false, '§');
 
     public final double movementSpeed; // speed factor
     public final boolean blocksMovement; // completely prevents walking
