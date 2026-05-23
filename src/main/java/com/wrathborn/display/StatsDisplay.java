dae46dbd19d11d8c8c93b56a4fd485f2ba1a8923
Bohdan Poruchnik <bogomdannyibogdan@gmail.com>
2026-05-24 00:06:19 +0300

    feat: added StatsDisplay class for displaying game stats

---
+++ src/main/java/com/wrathborn/display/StatsDisplay.java
@@ -0,0 +1,8 @@
+package com.wrathborn.display;
+
+public class StatsDisplay {
+    public void displayStats() {
+        System.out.println("\nGame Stats");
+        System.out.println("Status: Running");
+    }
+}
