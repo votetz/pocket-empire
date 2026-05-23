5099b2784d1c2af5d358e98b6e3c5b377c372fe6
Bohdan Poruchnik <bogomdannyibogdan@gmail.com>
2026-05-23 23:53:16 +0300

    refactor: simplify TileFactory

--- src/main/java/com/wrathborn/tiles/TileFactory.java
+++ src/main/java/com/wrathborn/tiles/TileFactory.java
@@ -1,36 +1,9 @@
 package com.wrathborn.tiles;
 
+import com.wrathborn.world.Tile;
+
 public class TileFactory {
-    public static Tile createTile(TileType type) {
-        return new Tile(type);
-    }
-
-    public static class Tile {
-        private final TileType type;
-
-        private Tile(TileType type) {
-            this.type = type;
-        }
-
-        public TileType getType() {
-            return type;
-        }
-
-        public double getMovementSpeed() {
-            return type.movementSpeed;
-        }
-
-        public boolean isImpassable() {
-            return type.blocksMovement;
-        }
-
-        public char getSymbol() {
-            return type.symbol;
-        }
-
-        @Override
-        public String toString() {
-            return String.valueOf(type.symbol);
-        }
+    public static Tile create(int x, int y, TileType type) {
+        return new Tile(x, y, type);
     }
 }
