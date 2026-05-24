13083feeda3098d598e03c4b7c70c3fa91151e71
Bohdan Poruchnik <bogomdannyibogdan@gmail.com>
2026-05-24 03:52:11 +0300

    feat: added UnitType enum and corresponding classes

---
+++ src/main/java/com/wrathborn/units/UnitType.java
@@ -0,0 +1,14 @@
+package com.wrathborn.units;
+
+public enum UnitType {
+    // combat units
+    LIGHT,
+    ARCHER,
+    HEAVY,
+    MAGE,
+    SIEGE,
+
+    // peaceful units
+    SETTLER,
+    WORKER
+}
