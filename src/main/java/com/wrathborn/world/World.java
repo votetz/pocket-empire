dff345214bb81f33909e3c7b900d390d376446c1
Bohdan Poruchnik <bogomdannyibogdan@gmail.com>
2026-05-26 11:56:16 +0300

    feat: updated World constructor to accept factions

--- src/main/java/com/wrathborn/world/World.java
+++ src/main/java/com/wrathborn/world/World.java
@@ -1,15 +1,33 @@
 package com.wrathborn.world;
 
+import com.wrathborn.entities.Unit;
+import com.wrathborn.entities.Faction;
+import java.util.List;
+import java.util.ArrayList;
+
 public class World {
     private final Map map;
+    private List<Faction> factions;
 
-    public World(Map map) {
+    public World(Map map, List<Faction> factions) {
         this.map = map;
+        this.factions = factions;
     }
 
     public Map getMap() {
         return map;
     }
 
+    public List<Faction> getFactions() {
+        return factions;
+    }
+
+    public List<Unit> getAllUnits() {
+        List<Unit> units = new ArrayList<>();
+        for (Faction faction : factions) {
+            units.addAll(faction.getUnits());
+        }
+        return units;
+    }
     void update() {}
 }
