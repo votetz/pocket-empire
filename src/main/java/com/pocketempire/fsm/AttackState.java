a6f11c665165699db713322633c39fe74337bb5b
Bohdan Poruchnik <bogomdannyibogdan@gmail.com>
2026-06-01 19:42:22 +0300

    feat: added AttackState class with combat resolution and state transitions

--- src/main/java/com/pocketempire/fsm/AttackState.java
+++ src/main/java/com/pocketempire/fsm/AttackState.java
@@ -1,5 +1,41 @@
 package com.pocketempire.fsm;
 
-public class AttackState {
+import com.pocketempire.entities.Faction;
+import com.pocketempire.entities.Unit;
+import com.pocketempire.world.HexUtils;
+import com.pocketempire.simulation.CombatResolver;
 
+import java.util.Comparator;
+import java.util.List;
+
+public class AttackState implements State {
+    @Override
+    public void enter(Unit unit) {
+        System.out.println(unit.getId() + " is now ATTACKING");
+    }
+
+    @Override
+    public void update(Unit unit, List<Unit> allUnits) {
+        Unit enemy = allUnits.stream()
+                .filter(u -> !u.getFactionId().equals(unit.getFactionId()))
+                .filter(Unit::isAlive)
+                .min(Comparator.comparingInt(u -> HexUtils.getDistance(
+                        unit.getQ(), unit.getR(), u.getQ(), u.getR())))
+                .orElse(null);
+
+        if (enemy != null) {
+            int dist = HexUtils.getDistance(
+                    unit.getQ(), unit.getR(), enemy.getQ(), enemy.getR());
+            if (dist <= unit.getRange()) {
+                CombatResolver.resolveCombat(unit, enemy);
+            }
+        }
+
+        unit.changeState(new IdleState(), UnitState.IDLE);
+    }
+
+    @Override
+    public void exit(Unit unit) {
+
+    }
 }
