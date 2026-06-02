0509ab9793c94b6fdd8e324a8a7a3b88434f4f55
Bohdan Poruchnik <bogomdannyibogdan@gmail.com>
2026-06-02 17:34:29 +0300

    feat: updated unit AI to include flee state handling

--- src/main/java/com/pocketempire/fsm/IdleState.java
+++ src/main/java/com/pocketempire/fsm/IdleState.java
@@ -1,11 +1,14 @@
 package com.pocketempire.fsm;
 
 import com.pocketempire.entities.Unit;
-import java.util.Comparator;
-import java.util.List;
 import com.pocketempire.world.HexUtils;
+import com.pocketempire.world.World;
+
+import java.util.Comparator;
 
 public class IdleState implements State {
+
+    private static final double FLEE_THRESHOLD = 0.5;
 
     @Override
     public void enter(Unit unit) {
@@ -13,8 +16,13 @@ public class IdleState implements State {
     }
 
     @Override
-    public void update(Unit unit, List<Unit> allUnits) {
-        Unit enemy = findNearestEnemy(unit, allUnits);
+    public void update(Unit unit, World world) {
+        if (unit.getHp() <= unit.getMaxHp() * FLEE_THRESHOLD) {
+            unit.changeState(new FleeState(), UnitState.FLEEING);
+            return;
+        }
+
+        Unit enemy = findNearestEnemy(unit, world.getAllUnits());
         if (enemy != null) {
             unit.changeState(new AttackState(), UnitState.ATTACKING);
         }
@@ -25,7 +33,7 @@ public class IdleState implements State {
 
     }
 
-    private Unit findNearestEnemy(Unit unit, List<Unit> allUnits) {
+    private Unit findNearestEnemy(Unit unit, java.util.List<Unit> allUnits) {
         return allUnits.stream()
                 .filter(u -> !u.getFactionId().equals(unit.getFactionId()))
                 .filter(Unit::isAlive)
