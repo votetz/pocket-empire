f5ff8b3416aaa3ae1fa03ea7dd18b0b0a72bf683
Bohdan Poruchnik <bogomdannyibogdan@gmail.com>
2026-06-07 16:38:00 +0300

    refactor: simplify unit type enum and remove unused fields

--- src/main/java/com/pocketempire/units/UnitType.java
+++ src/main/java/com/pocketempire/units/UnitType.java
@@ -1,23 +1,11 @@
 package com.pocketempire.units;
 
-import lombok.Getter;
-
 public enum UnitType {
-    // combat units
-    LIGHT(5),
-    ARCHER(6),
-    HEAVY(8),
-    MAGE(8),
-    SIEGE(8),
-
-    // peaceful units
-    SETTLER(4),
-    WORKER(5);
-
-    @Getter
-    private final int cost;
-
-    UnitType(int cost) {
-        this.cost = cost;
-    }
+    LIGHT,
+    ARCHER,
+    HEAVY,
+    MAGE,
+    SIEGE,
+    SETTLER,
+    WORKER
 }
