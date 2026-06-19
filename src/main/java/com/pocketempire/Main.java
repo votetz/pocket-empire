65d06134d8e8ad4637e5547379fd11c41a71736a
Bohdan Poruchnik <bogomdannyibogdan@gmail.com>
2026-06-19 13:17:47 +0300

    chore: resized the map

--- src/main/java/com/pocketempire/Main.java
+++ src/main/java/com/pocketempire/Main.java
@@ -5,7 +5,7 @@ import com.pocketempire.simulation.GameRunner;
 
 public class Main {
     public static void main(String[] args) {
-        GameSetup setup = new GameSetup(50, 15);
+        GameSetup setup = new GameSetup(35, 35);
         setup.setup();
 
         GameRunner runner = new GameRunner(setup);
