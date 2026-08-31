a0f8afd5b0158bdbc0325cb3a158866c6fc98c64
bohdan <bohdanporuchnik@gmail.com>
2026-08-31 12:25:10 +0000

    refactor: migrate tiles package to Kotlin

--- src/main/java/com/pocketempire/tiles/TileFactory.java
+++
@@ -1,9 +0,0 @@
-package com.pocketempire.tiles;
-
-import com.pocketempire.world.Tile;
-
-public class TileFactory {
-    public static Tile create(int x, int y, TileType type) {
-        return Tile.builder().q(x).r(y).type(type).improved(false).build();
-    }
-}
---
+++ src/main/kotlin/com/pocketempire/tiles/TileFactory.kt
@@ -0,0 +1,4 @@
+package com.pocketempire.tiles
+
+class TyleFactory {
+}
